bb77adbc88fb6461315c246371323b765fca111f
milkygraph <kenanbanda2004@gmail.com>
2025-03-25 14:17:49 +0300

    add borders to cmp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,13 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
   },
+  ui = {
+    border = "double",
+    size = {
+      width = 0.8,
+      height = 0.8,
+    },
+  },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
